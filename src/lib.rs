d5d72d3c21c8ae481aa05c32f7d85cfd3a538b3e
Aran Varun <vir@tue.su>
2025-02-28 22:04:41 +0300

    It works. TODO: parse and trim current location

--- src/lib.rs
+++ src/lib.rs
@@ -1,17 +1,36 @@
+use serde::{Deserialize, Serialize};
 use wasm_bindgen::prelude::*;
+
+#[derive(Deserialize, Serialize, Debug)]
+struct Claims {
+    custom_claim: String,
+    iss: String,
+    sub: String,
+    aud: String,
+    exp: u64,
+}
+
+#[wasm_bindgen]
+pub fn create_token(current_location: &str, ident_header: &str, ident_secret: &str) {
+    // create the claim
+    let c = Claims {
+        custom_claim: "DocPortal".to_string(),
+        iss: current_location.to_string(),
+        sub: ident_header.to_string(),
+        aud: ident_secret.to_string(),
+        exp: 1000,
+    };
+    // create the header
+    let header = jsonwebtoken::Header::default();
+    // create the encoding key using the secret string
+    let secret = jsonwebtoken::EncodingKey::from_secret("ecpkdocs".as_bytes());
+    // encode token
+    let token = jsonwebtoken::encode(&header, &c, &secret).unwrap();
+    log(&format!("{}/?jwt={}", current_location, token));
+}
 
 #[wasm_bindgen]
 extern "C" {
     #[wasm_bindgen(js_namespace = console)]
     pub fn log(s: &str);
-}
-
-#[wasm_bindgen]
-pub fn calcloc(curloc: &str) -> String {
-    format!("The current location is {}", curloc)
-}
-
-#[wasm_bindgen]
-pub fn greet(txt: &str) {
-    log(&format!("Hello! {}", txt));
 }
